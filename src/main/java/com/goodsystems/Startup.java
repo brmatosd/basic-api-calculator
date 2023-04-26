363d92555ceea26f2aed0f4905acbb71e8d83ed9
brmatosd <dan.souza.matos@gmail.com>
2023-04-26 12:46:17 -0300

    Updated pom and Startup files

--- src/main/java/com/goodsystems/Startup.java
+++ src/main/java/com/goodsystems/Startup.java
@@ -2,9 +2,10 @@ package com.goodsystems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Startup {
+public class Startup extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
